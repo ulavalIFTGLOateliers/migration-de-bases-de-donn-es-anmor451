4861de4259e8d14029288781536acf23d4138cee
SacFeve <antmorin374@outlook.com>
2024-03-08 12:25:46 -0500

    BugFix de la migration et du retour arrière

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -1,9 +1,9 @@
 ALTER TABLE singer RENAME TO musician;
+ALTER TABLE musician RENAME COLUMN singerName TO musicianName;
+ALTER TABLE musician ADD COLUMN role varchar(50);
+ALTER TABLE musician ADD COLUMN bandName varchar(50);
 CREATE TABLE band (bandName varchar(50), creation YEAR, genre varchar(50), PRIMARY KEY (bandName));
 INSERT INTO band VALUES ('Crazy Duo', 2015, 'rock'),('Luna', 2009, 'classical'), ('Mysterio', 2019, 'pop');
-ALTER TABLE musician RENAME COLUMN singerName TO musicianName,
-                     ADD COLUMN role varchar(50),
-                     ADD COLUMN bandName varchar(50);
 UPDATE musician SET bandName = 'Crazy Duo', role = 'vocals' WHERE musician.musicianName = 'Alina';
 UPDATE musician SET bandName = 'Crazy Duo', role = 'percussion' WHERE musician.musicianName = 'Rainbow';
 UPDATE musician SET bandName = 'Mysterio', role = 'guitar' WHERE musician.musicianName = 'Mysterio';
--- db_scripts/rollback_1.sql
+++ db_scripts/rollback_1.sql
@@ -1,6 +1,5 @@
 ALTER TABLE musician RENAME singer;
-ALTER TABLE singer
-    RENAME COLUMN musicianName to singerName,
-    DROP COLUMN role,
-    DROP COLUMN bandName;
+ALTER TABLE singer RENAME COLUMN musicianName to singerName;
+ALTER TABLE singer DROP COLUMN role;
+ALTER TABLE singer DROP COLUMN bandName;
 DROP TABLE band;
